e9935e99b0aec693eb460d129e60543b7e2b1d8d
KaYaF <d.guitar.c@gmail.com>
2022-08-15 00:09:07 +0900

    feat: update initial settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,16 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:nuxt/recommended',
     'prettier',
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
-}
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-useless-constructor': 'off',
+    'vue/multi-word-component-names': 'off',
+  },
+};
