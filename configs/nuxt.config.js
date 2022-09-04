6b4130ff00f76b35102a0cafce31d526f4c6d6c3
Ka <d.guitar.c@gmail.com>
2022-09-04 20:50:32 +0900

    chore: update nuxt.config.js

--- configs/nuxt.config.js
+++ configs/nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors';
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - vue-composition-sample',
+    titleTemplate: '%s - nuxt-sample',
     title: 'vue-composition-sample',
     htmlAttrs: {
       lang: 'en',
