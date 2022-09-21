889e91020cdb09681697e3a47ed924fff3ed70ab
KaYaF <d.guitar.c@gmail.com>
2022-09-21 15:13:57 +0900

    feat: add memo template

---
+++ components/L4_Template/L4_01_PageTemplate/MemoTemplate.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="memo-template"></div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'MemoTemplate',
+  setup() {},
+});
+</script>
--- pages/memo/index.vue
+++ pages/memo/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="memo-page"></div>
+  <div class="memo-page">
+    <MemoTemplate />
+  </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from '@nuxtjs/composition-api';
+import MemoTemplate from '~/components/L4_Template/L4_01_PageTemplate/MemoTemplate.vue';
 
 export default defineComponent({
   name: 'MemoPage',
+  components: {MemoTemplate},
   setup() {},
 });
 </script>
