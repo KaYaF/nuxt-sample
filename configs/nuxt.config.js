5a12b0b83a0241aa7da481bb470aa7933429b219
KaYaF <d.guitar.c@gmail.com>
2022-09-21 18:51:53 +0900

    chore: move variable.scss

--- configs/nuxt.config.js
+++ configs/nuxt.config.js
@@ -52,7 +52,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/scss/variables.scss'],
     theme: {
       dark: true,
       themes: {
