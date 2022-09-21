56bcccd8f7bcc443e580e236fb5420324ad31eef
KaYaF <d.guitar.c@gmail.com>
2022-09-21 15:13:10 +0900

    feat: add memo page

---
+++ pages/memo/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="memo-page"></div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'MemoPage',
+  setup() {},
+});
+</script>
