938abd22afbbb70d6f982888949d4463b6415e9b
KaYaF <d.guitar.c@gmail.com>
2022-09-21 15:26:54 +0900

    feat: add sample of the content of memo

---
+++ components/L3_Organisms/L3_02_Item/MemoCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="memo-card">
+    <v-card>
+      <!-- use L1 and L2 later -->
+      <v-card-title> memo: title </v-card-title>
+      <v-divider class="mx-4"></v-divider>
+      <v-card-text>
+        This is the {{ index }}-th memo.<br />
+        We can get the props and returned properties of the setup method.<br />
+        <br />
+        Test: squared index number is {{ squaredIndex }}
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'MemoCard',
+  props: {
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    return {squaredIndex: Math.pow(props.index, 2)};
+  },
+});
+</script>
--- components/L4_Template/L4_01_PageTemplate/MemoTemplate.vue
+++ components/L4_Template/L4_01_PageTemplate/MemoTemplate.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="memo-template"></div>
+  <div class="memo-template">
+    <MemoCards />
+  </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from '@nuxtjs/composition-api';
+import MemoCards from '~/components/L3_Organisms/L3_01_Layout/MemoCards.vue';
 
 export default defineComponent({
   name: 'MemoTemplate',
+  components: {MemoCards},
   setup() {},
 });
 </script>
