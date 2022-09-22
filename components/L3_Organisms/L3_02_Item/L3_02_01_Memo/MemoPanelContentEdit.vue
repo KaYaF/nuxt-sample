bffe32eee470b940daf738873382075aa3dcf1c4
KaYaF <d.guitar.c@gmail.com>
2022-09-22 17:59:16 +0900

    fix: apply validator

--- components/L3_Organisms/L3_02_Item/L3_02_01_Memo/MemoPanelContentEdit.vue
+++ components/L3_Organisms/L3_02_Item/L3_02_01_Memo/MemoPanelContentEdit.vue
@@ -1,46 +1,48 @@
 <template>
   <div class="memo-panel-content-edit">
-    <div class="memo-panel-content">
-      <title-small text="title:" />
-      <ValidationProvider v-slot="{errors}" name="title" rules="required">
-        <v-text-field
-          v-model="memoEdit.title"
-          outlined
-          placeholder="input title"
-          single-line
-          :error-messages="errors"
-        />
-      </ValidationProvider>
+    <ValidationObserver ref="editFormRef">
+      <div class="memo-panel-content">
+        <title-small text="title:" />
+        <ValidationProvider v-slot="{errors}" name="title" rules="required">
+          <v-text-field
+            v-model="memoEdit.title"
+            outlined
+            placeholder="input title"
+            single-line
+            :error-messages="errors"
+          />
+        </ValidationProvider>
 
-      <title-small text="content:" />
-      <ValidationProvider v-slot="{errors}" name="content" rules="required">
-        <v-textarea
-          v-model="memoEdit.content"
-          outlined
-          placeholder="input content"
-          :error-messages="errors"
-        />
-      </ValidationProvider>
-    </div>
-    <div class="memo-panel-footer d-flex justify-end mb-2">
-      <TextButton class="px-2" @onClick="updateMemo"> Edit </TextButton>
-      <TextButton class="px-2" color="accent" @onClick="cancel">
-        Cancel
-      </TextButton>
-    </div>
+        <title-small text="content:" />
+        <ValidationProvider v-slot="{errors}" name="content" rules="required">
+          <v-textarea
+            v-model="memoEdit.content"
+            outlined
+            placeholder="input content"
+            :error-messages="errors"
+          />
+        </ValidationProvider>
+      </div>
+      <div class="memo-panel-footer d-flex justify-end mb-2">
+        <TextButton class="px-2" @onClick="updateMemo"> Edit </TextButton>
+        <TextButton class="px-2" color="accent" @onClick="cancel">
+          Cancel
+        </TextButton>
+      </div>
+    </ValidationObserver>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent, PropType, ref} from '@nuxtjs/composition-api';
-import {ValidationProvider} from 'vee-validate';
+import {ValidationProvider, ValidationObserver} from 'vee-validate';
 import TextButton from '~/components/L1_Atom/L1_02_Button/TextButton.vue';
 import TitleSmall from '~/components/L1_Atom/L1_03_Text/TitleSmall.vue';
 import {Memo} from '~/types/contents/Memo';
 
 export default defineComponent({
   name: 'MemoPanelContentEdit',
-  components: {TextButton, TitleSmall, ValidationProvider},
+  components: {TextButton, TitleSmall, ValidationProvider, ValidationObserver},
   props: {
     memo: {
       type: Object as PropType<Memo>,
@@ -50,9 +52,14 @@ export default defineComponent({
   setup(props, context) {
     const memoEdit = ref<Memo>(props.memo);
     const memoOrigin = Object.assign({}, memoEdit.value);
+    const editFormRef = ref<InstanceType<typeof ValidationObserver>>();
 
     const updateMemo = () => {
-      context.emit('updateMemo', memoEdit.value);
+      editFormRef.value?.validate().then((success: boolean) => {
+        if (success) {
+          context.emit('updateMemo', memoEdit.value);
+        }
+      });
     };
 
     const cancel = () => {
@@ -60,7 +67,7 @@ export default defineComponent({
       context.emit('cancel');
     };
 
-    return {memoEdit, updateMemo, cancel};
+    return {memoEdit, editFormRef, updateMemo, cancel};
   },
 });
 </script>
