e8e1e8378844be2cc3b037a4f41d4295ed4898a6
KaYaF <d.guitar.c@gmail.com>
2022-09-23 23:47:26 +0900

    feat: use script setup on MdiIcon.vue

--- components/L1_Atom/L1_01_Icon/MdiIcon.vue
+++ components/L1_Atom/L1_01_Icon/MdiIcon.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+// name: 'MdiIcon'
+import {ICON_SIZE} from '~/constants/common';
+
+type Props = {
+  iconName: string;
+  size?: number;
+};
+
+withDefaults(defineProps<Props>(), {
+  size: ICON_SIZE,
+});
+</script>
+
 <template>
   <div class="mdi-icon">
     <v-icon :aria-label="`${iconName}-icon`" :size="size" aria-hidden="true"
@@ -5,23 +19,3 @@
     >
   </div>
 </template>
-
-<script lang="ts">
-import {defineComponent} from '@nuxtjs/composition-api';
-import {ICON_SIZE} from '~/constants/common';
-
-export default defineComponent({
-  name: 'MdiIcon',
-  props: {
-    iconName: {
-      type: String,
-      required: true,
-    },
-    size: {
-      type: Number,
-      default: ICON_SIZE,
-    },
-  },
-  setup() {},
-});
-</script>
