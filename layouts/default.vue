e41a13e363260e03d7de92ee030b533712c7238d
KaYaF <d.guitar.c@gmail.com>
2022-09-24 01:52:33 +0900

    feat: use script setup on layouts files

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,49 +1,42 @@
-<template>
-  <v-app dark>
-    <Sidebar :items="items" />
-    <v-app-bar app>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer />
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
-</template>
-
-<script lang="ts">
-import {defineComponent} from '@nuxtjs/composition-api';
+<script setup lang="ts">
+// name: 'DefaultLayout',
 import Sidebar from '~/components/L3_Organism/L3_01_Layout/Sidebar.vue';
 import {ROUTE} from '~/constants/route';
 import {SidebarItem} from '~/types/contents/Sidebar';
 
-export default defineComponent({
-  name: 'DefaultLayout',
-  components: {Sidebar},
-  setup() {
-    const items: SidebarItem[] = [
-      {
-        icon: 'mdi-apps',
-        title: 'Welcome',
-        to: ROUTE.home,
-      },
-      {
-        icon: 'mdi-book-open-page-variant',
-        title: 'memo',
-        to: ROUTE.memo,
-      },
-    ];
-
-    return {
-      items,
-      title: 'Nuxt Sample',
-    };
+const items: SidebarItem[] = [
+  {
+    icon: 'mdi-apps',
+    title: 'Welcome',
+    to: ROUTE.home,
   },
-});
+  {
+    icon: 'mdi-book-open-page-variant',
+    title: 'memo',
+    to: ROUTE.memo,
+  },
+];
+
+const title = 'Nuxt Sample';
 </script>
+
+<template>
+  <div class="default-layout">
+    <v-app dark>
+      <Sidebar :items="items" />
+      <v-app-bar app>
+        <v-toolbar-title>{{ title }}</v-toolbar-title>
+        <v-spacer />
+      </v-app-bar>
+      <v-main>
+        <v-container>
+          <Nuxt />
+        </v-container>
+      </v-main>
+
+      <v-footer app>
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+      </v-footer>
+    </v-app>
+  </div>
+</template>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+import {defineComponent} from 'vue';
+import {useNuxt2Meta} from '@nuxt/bridge/dist/runtime';
+
+defineComponent({
+  name: 'ErrorLayout',
+  layout: 'error',
+  head: {},
+});
+
+const props = defineProps({
+  error: {
+    type: Object,
+    default: null,
+  },
+});
+
+const pageNotFound = '404 Not Found';
+const otherError = 'An error occurred';
+
+useNuxt2Meta({
+  title: props.error.statusCode === 404 ? pageNotFound : otherError,
+});
+</script>
+
 <template>
   <v-app dark>
     <h1 v-if="error.statusCode === 404">
@@ -9,35 +34,6 @@
     <NuxtLink to="/"> Home page </NuxtLink>
   </v-app>
 </template>
-
-<script lang="ts">
-import {defineComponent, useMeta} from '@nuxtjs/composition-api';
-
-export default defineComponent({
-  name: 'ErrorLayout',
-  layout: 'error',
-  props: {
-    error: {
-      type: Object,
-      default: null,
-    },
-  },
-  setup(props) {
-    const pageNotFound = '404 Not Found';
-    const otherError = 'An error occurred';
-
-    useMeta({
-      title: props.error.statusCode === 404 ? pageNotFound : otherError,
-    });
-
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
-    };
-  },
-  head: {},
-});
-</script>
 
 <style scoped>
 h1 {
