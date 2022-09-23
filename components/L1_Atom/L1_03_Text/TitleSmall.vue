321f4fcc0b1faf30b3b26e95c6f4cbda55c49729
KaYaF <d.guitar.c@gmail.com>
2022-09-24 00:39:42 +0900

    feat: use script setup on TitleSmall.vue

--- components/L1_Atom/L1_03_Text/TitleSmall.vue
+++ components/L1_Atom/L1_03_Text/TitleSmall.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+// name: 'TitleSmall'
+
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
 <template>
   <div class="title-small">
     <div class="fs-14">
@@ -5,21 +16,6 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import {defineComponent} from '@nuxtjs/composition-api';
-
-export default defineComponent({
-  name: 'TitleSmall',
-  props: {
-    text: {
-      type: String,
-      required: true,
-    },
-  },
-  setup() {},
-});
-</script>
 
 <style lang="scss" scoped>
 @import '~/assets/scss/Text/TextSize.scss';
